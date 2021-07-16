e21aefee0930a783c993aa94726f1837795a9d98
Nome-dev <76884445+Nome-dev@users.noreply.github.com>
2021-07-15 23:36:42 -0300

    Update a2.js

--- The_Upgrade_Project_1/a2.js
+++ The_Upgrade_Project_1/a2.js
@@ -10,34 +10,7 @@ function verificar1()
     alert("Verificar1");
 }
 
-function onload_Body()
-{
-    console.log("Teste simples de soma:");
-    
-    console.log("Entrada 1:");
-    
-    var n1 = console.readLine();
-    
-    console.log("Entrada 2:");
-    
-    var n2 = console.readLine();
-    
-    console.log("Resultado:");
-    console.log(n1 + n2);
-}
-
 function start()
 {
-    console.log("Teste simples de soma:");
     
-    console.log("Entrada 1:");
-    
-    var n1 = console.readLine();
-    
-    console.log("Entrada 2:");
-    
-    var n2 = console.readLine();
-    
-    console.log("Resultado:");
-    console.log(n1 + n2);
 }

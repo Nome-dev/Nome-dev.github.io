5d54ada6f0576a7f10ac24828819626c443411b0
Nome-dev <gastrunkis16@gmail.com>
2021-07-16 22:16:17 -0300

    c

--- The_Upgrade_Project_1/a2.js
+++ The_Upgrade_Project_1/a2.js
@@ -1,3 +1,6 @@
+var p2 = document.getElementById("p2");
+var tb1 = document.getElementById("textBox1");
+
 start();
 
 function button1_Click()
@@ -7,7 +10,9 @@ function button1_Click()
 
 function verificar1()
 {
-    
+    var value1 = tb1.textContent;
+
+    alert(value1);
 }
 
 function start()

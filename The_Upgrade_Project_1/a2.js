5d2e2549c76798fa96587c1001b75535b2acf1b1
Nome-dev <gastrunkis16@gmail.com>
2021-07-18 20:56:24 -0300

    c

--- The_Upgrade_Project_1/a2.js
+++ The_Upgrade_Project_1/a2.js
@@ -1,6 +1,8 @@
 var p2 = document.getElementById("p2");//Variável que representa o objeto de saída
 var tb1 = document.getElementById("textBox1");//Variável que representa o objeto de entrada
 
+var out1 = "";
+
 start();//Inicializa a função start()
 
 function button1_Click()//É executado quando o botão com id="button1" é pressionado
@@ -13,6 +15,8 @@ function verificar1()//Função principal da ação
     var value1 = tb1.value;//Variável que representa o valor de entrada, na forma de conjunto de caracteres
     
     verificar2(value1);
+
+    aplicar1();
 }
 
 function start()
@@ -20,7 +24,12 @@ function start()
     
 }
 
+function aplicar1()
+{
+    p2.value = out1;
+}
+
 function verificar2(value1)
 {
-    
+    out1 = value1;
 }
